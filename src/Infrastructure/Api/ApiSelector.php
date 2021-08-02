58fd7476e7243ac40725b6250669f728441b2435
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-08-02 17:58:15 +0200

    Implement all APIs of the CE version.

--- src/Infrastructure/Api/ApiSelector.php
+++ src/Infrastructure/Api/ApiSelector.php
@@ -4,6 +4,7 @@ namespace AkeneoEtl\Infrastructure\Api;
 
 
 use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
+use LogicException;
 
 class ApiSelector
 {
@@ -13,9 +14,30 @@ class ApiSelector
     public function getApi(AkeneoPimClientInterface $client, string $dataType)
     {
         $supportedApis = [
-            'product'       => function() use ($client) { return $client->getProductApi(); },
-            'product-model' => function() use ($client) { return $client->getProductModelApi(); },
+            'association-type'   => function() use ($client) { return $client->getAssociationTypeApi(); },
+            'category'           => function() use ($client) { return $client->getCategoryApi(); },
+            'channel'            => function() use ($client) { return $client->getChannelApi(); },
+            'currency'           => function() use ($client) { return $client->getCurrencyApi(); },
+            'locale'             => function() use ($client) { return $client->getLocaleApi(); },
+
+            'attribute'          => function() use ($client) { return $client->getAttributeApi(); },
+            'attribute-group'    => function() use ($client) { return $client->getAttributeGroupApi(); },
+            'attribute-option'   => function() use ($client) { return $client->getAttributeOptionApi(); },
+
+            'product'            => function() use ($client) { return $client->getProductApi(); },
+            'product-model'      => function() use ($client) { return $client->getProductModelApi(); },
+
+            'family'             => function() use ($client) { return $client->getFamilyApi(); },
+            'family-variant'     => function() use ($client) { return $client->getFamilyVariantApi(); },
+
+            'measure-family'     => function() use ($client) { return $client->getMeasureFamilyApi(); },
+            'measurement-family' => function() use ($client) { return $client->getMeasurementFamilyApi(); },
         ];
+
+        if (isset($supportedApis[$dataType]) === false) {
+            throw new LogicException(sprintf('Unsupported data type: %s. Please, select one of following types: %s.',
+                $dataType, implode(', ', array_keys($supportedApis))));
+        }
 
         return $supportedApis[$dataType]();
     }
