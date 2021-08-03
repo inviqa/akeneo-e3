6e631a8d909736f5d871f1fb015baf71c31184fb
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-08-03 23:15:05 +0200

    Rename command.
    It can process all types now.

--- src/Infrastructure/Command/TransformCommand.php
+++ src/Infrastructure/Command/TransformCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class TransformProductsCommand extends Command
+class TransformCommand extends Command
 {
     private EtlFactory $factory;
     private ConnectionProfileFactory $connectionProfileFactory;
