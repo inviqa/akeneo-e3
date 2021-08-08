2c5e5a77acdadba01945a0312f567f8ad80df196
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-08-08 14:29:17 +0200

    Implement progress object.

---
+++ src/Domain/Transform/Progress.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AkeneoEtl\Domain\Transform;
+
+class Progress
+{
+    private int $current;
+    private int $total;
+
+    private function __construct(int $total)
+    {
+        $this->current = 0;
+        $this->total = $total;
+    }
+
+    public static function create(int $total): self
+    {
+        return new self($total);
+    }
+
+    public function current(): int
+    {
+        return $this->current;
+    }
+
+    public function total(): int
+    {
+        return $this->total;
+    }
+
+    public function advance(): self
+    {
+        $this->current++;
+
+        return $this;
+    }
+}
