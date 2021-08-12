be679b81ddd8fdd8157c7fb1b2623d35287a8b7a
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-08-12 11:12:55 +0200

    Get rid of closures in API selector.
    Now it returns links to APIs explicitly.

--- src/Infrastructure/Api/ApiSelector.php
+++ src/Infrastructure/Api/ApiSelector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace AkeneoEtl\Infrastructure\Api;
 
 use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
-use Akeneo\PimEnterprise\ApiClient\AkeneoPimEnterpriseClient;
 use Akeneo\PimEnterprise\ApiClient\AkeneoPimEnterpriseClientInterface;
 use LogicException;
 
@@ -17,84 +16,38 @@ final class ApiSelector
     public function getApi(AkeneoPimClientInterface $client, string $dataType)
     {
         $supportedApis = [
-            'association-type'   => function () use ($client) {
-                return $client->getAssociationTypeApi();
-            },
-            'category'           => function () use ($client) {
-                return $client->getCategoryApi();
-            },
-            'channel'            => function () use ($client) {
-                return $client->getChannelApi();
-            },
-            'currency'           => function () use ($client) {
-                return $client->getCurrencyApi();
-            },
-            'locale'             => function () use ($client) {
-                return $client->getLocaleApi();
-            },
+            'association-type'   => $client->getAssociationTypeApi(),
+            'category'           => $client->getCategoryApi(),
+            'channel'            => $client->getChannelApi(),
+            'currency'           => $client->getCurrencyApi(),
+            'locale'             => $client->getLocaleApi(),
 
-            'attribute'          => function () use ($client) {
-                return $client->getAttributeApi();
-            },
-            'attribute-group'    => function () use ($client) {
-                return $client->getAttributeGroupApi();
-            },
-            'attribute-option'   => function () use ($client) {
-                return $client->getAttributeOptionApi();
-            },
+            'attribute'          => $client->getAttributeApi(),
+            'attribute-group'    => $client->getAttributeGroupApi(),
+            'attribute-option'   => $client->getAttributeOptionApi(),
 
-            'product'            => function () use ($client) {
-                return $client->getProductApi();
-            },
-            'product-model'      => function () use ($client) {
-                return $client->getProductModelApi();
-            },
+            'product'            => $client->getProductApi(),
+            'product-model'      => $client->getProductModelApi(),
 
-            'family'             => function () use ($client) {
-                return $client->getFamilyApi();
-            },
-            'family-variant'     => function () use ($client) {
-                return $client->getFamilyVariantApi();
-            },
+            'family'             => $client->getFamilyApi(),
+            'family-variant'     => $client->getFamilyVariantApi(),
 
-            'measurement-family' => function () use ($client) {
-                return $client->getMeasurementFamilyApi();
-            },
+            'measurement-family' => $client->getMeasurementFamilyApi(),
         ];
 
         if ($client instanceof AkeneoPimEnterpriseClientInterface) {
             $supportedApis += [
-                'reference-entity'                  => function () use ($client) {
-                    return $client->getReferenceEntityApi();
-                },
-                'reference-entity-attribute'        => function () use ($client) {
-                    return $client->getReferenceEntityAttributeApi();
-                },
-                'reference-entity-attribute-option' => function () use ($client) {
-                    return $client->getReferenceEntityAttributeOptionApi();
-                },
-                'reference-entity-record'           => function () use ($client) {
-                    return $client->getReferenceEntityRecordApi();
-                },
-                'reference-entity-media-file'       => function () use ($client) {
-                    return $client->getReferenceEntityMediaFileApi();
-                },
+                'reference-entity'                  => $client->getReferenceEntityApi(),
+                'reference-entity-attribute'        => $client->getReferenceEntityAttributeApi(),
+                'reference-entity-attribute-option' => $client->getReferenceEntityAttributeOptionApi(),
+                'reference-entity-record'           => $client->getReferenceEntityRecordApi(),
+                'reference-entity-media-file'       => $client->getReferenceEntityMediaFileApi(),
 
-                'asset'                              => function () use ($client) {
-                    return $client->getAssetManagerApi();
-                },
-                'asset-attribute'                    => function () use ($client) {
-                    return $client->getAssetAttributeApi();
-                },
-                'asset-attribute-option'             => function () use ($client) {
-                    return $client->getAssetAttributeOptionApi();
-                },
-                'asset-family'                       => function () use ($client) {
-                    return $client->getAssetFamilyApi();
-                },
-                'asset-media-file'                   => function () use ($client) {
-                    return $client->getAssetMediaFileApi();
-                },
+                'asset'                             => $client->getAssetManagerApi(),
+                'asset-attribute'                   => $client->getAssetAttributeApi(),
+                'asset-attribute-option'            => $client->getAssetAttributeOptionApi(),
+                'asset-family'                      => $client->getAssetFamilyApi(),
+                'asset-media-file'                  => $client->getAssetMediaFileApi(),
             ];
         }
 
@@ -106,6 +59,6 @@ final class ApiSelector
             ));
         }
 
-        return $supportedApis[$dataType]();
+        return $supportedApis[$dataType];
     }
 }
